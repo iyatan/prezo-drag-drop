a874158d1b375a0bdccb0642cf5c2ca61b7d99ae
Omkar <rakmoomkar@gmail.com>
2023-02-02 11:53:43 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# prezo-assignment
+
+Hi there, thanks for your interest in Prezo. We are excited to work with builders and hackers to help us shape Prezo.
+
+This repo hosts a basic drag and drop interface that we built into Prezo. The version in this repo is incomplete, buggy and just not ready. Your mission, should you choose to accept it, is to create a working version of the drag and drop interface for slides.
+
+This is what it will look like when ready [Demo video](https://www.loom.com/share/be346974201f450f9dc7e62480ce83e6)
+
+## Requirements
+
+- get the drag and drop working
+
+- be able to scroll inside the slide container while dragging slides
+
+- fix minor css issues that you can notice
+
+## Advanced requirements
+
+- make sure the dropped slide is always visible in the viewport
+
+- allow dragging multiple slides at once
+
+- add undo/redo
+
+- any ux flair you want to add OR an implementation that you think is even better than using the HTML Drag and Drop API
+
+The project uses Vue 3 and Vite. You don't really need to know a lot about Vue to complete this. The official Vue docs are quite good if you want to quickly understand how to work with Vue.
+
+Please do not use any dragging related libaries.
+
+## Resources
+
+- [Drag and Drop API](https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API)
+
+- [Vue 3 Docs](https://vuejs.org/guide/introduction.html)
+
+### Technical stuff about running the code
+
+This template should help get you started developing with Vue 3 in Vite.
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
---
+++ src/views/DragView.vue
@@ -0,0 +1,142 @@
+<template>
+  <div
+    :style="containerStyle"
+    @drop="onDrop($event)"
+    @dragenter.prevent
+    @dragover.prevent
+    ref="containerRef"
+  >
+    <div
+      v-for="slide, index in slides"
+      :key="slide.id"
+      :style="slideContainerStyle"
+      ref="itemRefs"
+    >
+      <div
+        :style="slideStyle"
+        draggable="true"
+        @dragstart="handleDragStart($event, index)"
+        @dragover="handleDragOver($event, index)"
+        @dragleave="handleDragLeave($event, index)"
+      >
+        {{ slide.title }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable no-unused-vars */
+import { ref, onBeforeUpdate } from 'vue';
+import { useEventListener, useElementBounding } from '@vueuse/core';
+
+export default {
+  setup() {
+    const itemRefs = ref([]);
+
+    onBeforeUpdate(() => {
+      itemRefs.value = [];
+    });
+
+    const containerRef = ref(null);
+
+    const { top, bottom } = useElementBounding(containerRef);
+
+    useEventListener(containerRef, 'drag', (e) => {
+      // handle scroll here
+    });
+
+    return { itemRefs, containerRef };
+  },
+  data() {
+    return {
+      containerStyle: {
+        margin: '20px 20px',
+        width: '240px',
+        border: '1px solid black',
+        borderRadius: '4px',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '4px',
+        alignItems: 'center',
+        overflow: 'hidden auto',
+        height: '80vh',
+        padding: '8px',
+      },
+      slideContainerStyle: {
+        borderRadius: '4px',
+        padding: '8px',
+        width: '100%',
+        height: '130px',
+        position: 'relative',
+      },
+      slideStyle: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: '0 10px',
+        height: '114px',
+        borderRadius: '6px',
+        justifyContent: 'center',
+        background: '#FFF',
+        border: '1px solid #e5e8eb',
+        width: '100%',
+        color: 'black',
+      },
+      slides: [
+        {
+          id: '1',
+          title: 'Slide 1',
+        },
+        {
+          id: '2',
+          title: 'Slide 2',
+        },
+        {
+          id: '3',
+          title: 'Slide 3',
+        },
+        {
+          id: '4',
+          title: 'Slide 4',
+        },
+        {
+          id: '5',
+          title: 'Slide 5',
+        },
+        {
+          id: '6',
+          title: 'Slide 6',
+        },
+        {
+          id: '7',
+          title: 'Slide 7',
+        },
+        {
+          id: '8',
+          title: 'Slide 8',
+        },
+        {
+          id: '9',
+          title: 'Slide 9',
+        },
+        {
+          id: '10',
+          title: 'Slide 10',
+        },
+      ],
+    };
+  },
+  computed: {
+  },
+  methods: {
+    handleDragStart(event, index) {
+    },
+    onDrop(event) {
+    },
+    handleDragOver(event, index) {
+    },
+    handleDragLeave(event, index) {
+    },
+  },
+};
+</script>
